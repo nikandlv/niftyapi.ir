cd10cb4040a7d7f74c1d2d9253974b57ba6eba95
Nikan Dalvand <nikandalvand@gmail.com>
2019-07-31 23:48:08 +0430

    Added services title and links

--- components/Header/index.jsx
+++ components/Header/index.jsx
@@ -3,6 +3,13 @@ export default function Header() {
         <header>
             <span className="title" >NiftyAPI</span>
             <span className="description" >A fast, reliable API gateway</span>
+            <span className="services">Services</span>
+            <ul>
+            <li>Authentication</li>
+            <li>Analytics</li>
+            <li>Metrics</li>
+            <li>Crash reports</li>
+            </ul>
         </header>
     )
 }
