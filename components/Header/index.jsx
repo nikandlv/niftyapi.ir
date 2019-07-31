14f4272dc086a58c5d1c7f91299fb162ce010cca
Nikan Dalvand <nikandalvand@gmail.com>
2019-07-31 23:44:18 +0430

    Added header description

--- components/Header/index.jsx
+++ components/Header/index.jsx
@@ -2,6 +2,7 @@ export default function Header() {
     return (
         <header>
             <span className="title" >NiftyAPI</span>
+            <span className="description" >A fast, reliable API gateway</span>
         </header>
     )
 }
