b74a4be4a79f2f86b01d48c0f3aa18734847b7cb
Nikan Dalvand <nikandalvand@gmail.com>
2019-08-01 18:53:56 +0430

    Updated the styling of the services and its title

--- components/Header/index.jsx
+++ components/Header/index.jsx
@@ -3,12 +3,12 @@ export default function Header() {
         <header>
             <span className="title" >NiftyAPI</span>
             <span className="description" >A fast, reliable API gateway</span>
-            <span className="services">Services</span>
-            <ul>
-            <li>Authentication</li>
-            <li>Analytics</li>
-            <li>Metrics</li>
-            <li>Crash reports</li>
+            <span className="services_title">Services</span>
+            <ul class="services">
+                <li>Authentication</li>
+                <li>Analytics</li>
+                <li>Metrics</li>
+                <li>Crash reports</li>
             </ul>
         </header>
     )
